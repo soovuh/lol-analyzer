bcd5adfb8c2f89123a7510a412be85715a25b25a
Klymenko Illia <klymenko.illia.dev@gmail.com>
2024-03-19 13:25:10 +0200

    first setup

--- lol_analyzer/settings.py
+++ lol_analyzer/settings.py
@@ -3,23 +3,13 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,19 +51,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'lol_analyzer.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -91,9 +74,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -102,13 +82,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- riot_requests/players.py
+++ riot_requests/players.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+from requests.exceptions import HTTPError
 import cassiopeia as cass
 from dotenv import load_dotenv
 from riot_requests.champions import UserChampion
@@ -11,15 +12,51 @@ DEFAULT_LANGUAGE_REGION = 'EUW'
 cass.set_riot_api_key(API_KEY)
 
 
-class Player:
-    def __init__(self, name, region):
-        self.name = name
-        self.region = region
-        self.cass_summoner = cass.get_summoner(name=name, region=region)
-        self.puid = self.cass_summoner.puuid
+def get_user_puuid(nickname, tagline):
+    tagline = tagline.replace('#', '')
+    nickname = nickname.strip()
 
+    url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{nickname}/{tagline}'
+    headers = {'X-Riot-Token': API_KEY}
+    response = requests.get(url=url, headers=headers)
+
+    data = response.json()
+    
+    if response.status_code != 200:
+        raise HTTPError
+    
+    return data['puuid']
+
+def get_user_account_id(puuid, region):
+    region = region.lower()
+    
+    url = f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}'
+    headers = {'X-Riot-Token': API_KEY}
+    response = requests.get(url=url, headers=headers)
+
+    data = response.json()
+
+    if response.status_code != 200:
+        raise HTTPError
+    
+    return data['accountId']
+
+
+class Player:
+    def __init__(self, account_id, puuid, region, nickname):
+        self.account_id = account_id
+        self.puuid = puuid
+        self.region = region
+        self.nickname = nickname
+        self.summoner = cass.get_summoner(account_id=account_id, region=region)
+        
+        # Get needed fields from Summoner class in cass
+        self.level = self.summoner.level
+        self.profile_icon_url = self.summoner.profile_icon().url
+        print(self.profile_icon_url)
+        
     def get_top_5_mastery_champs(self):
-        url = f'https://{self.region.lower()}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{self.puid}?api_key={API_KEY}'
+        url = f'https://{self.region.lower()}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{self.puuid}?api_key={API_KEY}'
         response = requests.get(url)
 
         data = response.json()
